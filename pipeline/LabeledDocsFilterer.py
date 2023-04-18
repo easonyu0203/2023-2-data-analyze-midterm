9647956f3c6b4bc1ca2d679837a991234675321e
easonyu0203 <breaking0203@gmail.com>
2023-04-19 02:02:41 +0800

    docs: fix docs

--- pipeline/LabeledDocsFilterer.py
+++ pipeline/LabeledDocsFilterer.py
@@ -3,14 +3,13 @@ from typing import Protocol
 from tqdm import tqdm
 
 from datasets.labeled_docs_dataset import ILabeledDataset, LabelDataset
-from datasets.stock_dataset import Stock
 
 
 class ILabeledDocsFilterer(Protocol):
-    """Filter labeled documents that aren't relevant to the stock we are interested in."""
+    """filter docs by the label"""
 
-    def filter_documents(self, labeled_documents: ILabeledDataset, stock: Stock, verbose=True) -> ILabeledDataset:
-        """filter labeled documents by relevant to the stock we are interested in."""
+    def filter_documents(self, labeled_documents: ILabeledDataset, verbose=True) -> ILabeledDataset:
+        """filter docs by the label"""
         ...
 
 
@@ -20,8 +19,8 @@ class Near0returnFilterer(ILabeledDocsFilterer):
     def __init__(self, threshold: float = 5):
         self.threshold = threshold
 
-    def filter_documents(self, labeled_documents: ILabeledDataset, stock: Stock, verbose=True) -> ILabeledDataset:
-        stock_name = stock.name
+    def filter_documents(self, labeled_documents: ILabeledDataset, verbose=True) -> ILabeledDataset:
+        """filter out y (future return %) is close to 0"""
         filtered_documents = []
         labels = []
         pbar = tqdm(labeled_documents, desc="filtering documents", disable=not verbose)
