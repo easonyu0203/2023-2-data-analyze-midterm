63e3fb03e884831e2a41315893f6b3d27d8574af
easonyu0203 <breaking0203@gmail.com>
2023-04-18 10:36:05 +0800

    feat: add .py for organize data

---
+++ organize/organize_document_data.py
@@ -0,0 +1,114 @@
+# # Organize Article Dataset
+# The raw dataset is a little messy, in this notebook, we aim to process dataset to became more structured, here is main steps:
+# 1. Remove unused columns
+# 2. covert data types( exp: post_time to datetime)
+# 3. for each year/kind we make a new csv file
+# 4. each year stock data separate to a csv file
+
+
+import pandas as pd
+from pathlib import Path
+from typing import Literal
+import os
+
+# change to project root directory
+folder_name = "organize"
+if folder_name in os.getcwd():
+    os.chdir(os.path.abspath(os.pardir))
+
+# configurations
+DATA_DIR = "./bda2023_mid_dataset"
+ORGANIZED_DATASET_DIR = "./organized_data"
+ORGANIZED_DATASET_NAME = "documents.csv"
+RAW_DATASET_NAMES = {
+    'bbs_2019_2021': "bda2023_mid_bbs_2019-2021.csv",
+    'bbs_2022_2023': "bda2023_mid_bbs_2022-2023.csv",
+    'forum_2019': "bda2023_mid_forum_2019.csv",
+    'forum_2020': "bda2023_mid_forum_2020.csv",
+    'forum_2021': "bda2023_mid_forum_2021.csv",
+    'forum_2022_2023': "bda2023_mid_forum_2022-2023.csv",
+    'news_2019': "bda2023_mid_news_2019.csv",
+    'news_2020': "bda2023_mid_news_2020.csv",
+    'news_2021': "bda2023_mid_news_2021.csv",
+    'news_2022': "bda2023_mid_news_2022.csv",
+    'news_2022_2023': "bda2023_mid_news_2022-2023.csv",
+    'news_2023': "bda2023_mid_news_2023.csv",
+}
+
+
+# # Utility functions
+def have_same_columns(*dfs):
+    """check df have same columns or not"""
+    for i in range(len(dfs)):
+        for j in range(i + 1, len(dfs)):
+            if not dfs[i].columns.equals(dfs[j].columns):
+                print(f"columns of {i} and {j} are not equal")
+                return False
+    return True
+
+
+def get_article_dfs_by_type(article_type: Literal['bbs', 'forum', 'news']) -> pd.DataFrame:
+    """
+    get article dfs by type, type can be bbs, forum, news
+    return a dataframe that contains all data of that type.
+    exp: get_article_dfs_by_type('bbs') will return a dataframe that contains all bbs data from 2019 to 2023
+    """
+    raw_dataset_paths = {k: Path(DATA_DIR, v) for k, v in RAW_DATASET_NAMES.items()}
+    dfs = [pd.read_csv(file_path) for k, file_path in raw_dataset_paths.items() if k.startswith(article_type)]
+    assert have_same_columns(*dfs)
+    df = pd.concat(dfs)
+    return df
+
+
+# load all article df
+bbs_df = get_article_dfs_by_type('bbs')
+forum_df = get_article_dfs_by_type('forum')
+news_df = get_article_dfs_by_type('news')
+article_df = pd.concat([bbs_df, forum_df, news_df])
+
+# After some understanding of the dataset, we decided to do the following
+# 1. as three types of articles df is pretty much the same, so I decided to stack them together
+# 2. remove duplicates (by id)
+# 3. remove useless columns, we don't need id and page url
+# 4. sort by post_time (first converted to datetime object)
+# 
+# NOTE: we don't handle Null values here, because the way we handle null values depends on the algorithm we want to use.
+
+# # Preprocessing
+# remove duplicated rows by id
+original_rows = article_df.shape[0]
+article_df = article_df.drop_duplicates(subset=['id'])
+dropped_rows = original_rows - article_df.shape[0]
+print(f"{dropped_rows} rows were dropped due to duplicates.")
+
+# drop id and page url
+print("drop id and page url")
+article_df = article_df.drop(columns=['id', 'page_url'])
+
+# convert post_time to datetime object and sort by post_time
+print("convert post_time to datetime object and sort by post_time")
+article_df['post_time'] = pd.to_datetime(article_df['post_time'])
+article_df = article_df.sort_values(by='post_time')
+
+# if author or title or content is NaN, convert to ""
+article_df['author'].fillna('""', inplace=True)
+article_df['title'].fillna('""', inplace=True)
+article_df['content'].fillna('""', inplace=True)
+
+print("set post_time as index and drop duplicates from index")
+article_df.set_index('post_time', inplace=True)
+# check for duplicates in the index
+if article_df.index.duplicated().any():
+    # drop duplicates from the index
+    article_df = article_df[~article_df.index.duplicated()]
+
+
+# the preprocessed data seem good, store it to csv
+print("saving...")
+article_csv_path = Path(ORGANIZED_DATASET_DIR, ORGANIZED_DATASET_NAME)
+# create preprocessed_dataset dir if not exists
+if not Path(ORGANIZED_DATASET_DIR).exists():
+    Path(ORGANIZED_DATASET_DIR).mkdir()
+# save to csv file
+article_df.to_csv(article_csv_path)
+print(f"article df is saved to {article_csv_path}")
---
+++ organize/organize_stock_data.py
@@ -0,0 +1,111 @@
+# # Organize Stock Dataset Stock data is store in an Excel file but the structure isn't what we ideally want. In this
+# notebook we aim to do the following 1. make two Excel files one contain all 上市股票 other contain all 上櫃股票 2. separate
+# each stock to a sheet and order by time 3. rename column to english so will be easy to use in the future
+from typing import Dict
+
+import pandas as pd
+from pathlib import Path
+from tqdm import tqdm
+import os
+
+# change to project root directory
+folder_name = "organize"
+if folder_name in os.getcwd():
+    os.chdir(os.path.abspath(os.pardir))
+
+
+# # Configuration
+DATA_DIR = "./bda2023_mid_dataset"
+STOCKS_DIR = "./organized_data/stocks"
+METADATA_PATH = "./organized_data/stock_metadata.csv"
+RAW_DATASET_NAME = 'stock_data_2019-2023.xlsx'
+ORGANIZED_DATASET_DIR = './organized_data/stocks'
+RENAMED_COLUMNS = [
+    'name', 'date', 'open', 'high', 'low', 'close', 'volume(k)', 'turnover(k)', 'transaction', 'outstanding(k)', 'pe', 'pb'
+]
+raw_stocks_path = Path(DATA_DIR, RAW_DATASET_NAME)
+
+
+# # Utility functions
+def get_raw_stocks_dfs() -> Dict[str, pd.DataFrame]:
+    """
+    get raw stocks dfs, this function will load the excel file and return a dict of dfs
+    """
+    # Load the Excel file
+    excel_file = pd.ExcelFile(raw_stocks_path)
+
+    # Get the sheet names
+    sheet_names = excel_file.sheet_names
+
+    # Load all sheets
+    dfs = {sheet_name: excel_file.parse(sheet_name, na_values=['-']) for sheet_name in sheet_names if sheet_name != '摘要'}
+    return dfs
+
+
+# # Research & Preprocessing
+# I just simply open excel and look around a bit, here we check about null value and see
+# if there is any error in the data
+raw_stock_dfs = get_raw_stocks_dfs()
+
+# Look like there is some stock have missing prices value, we will mark them as null value and handle it later
+# first, let change column into english, and cast data type to what we want
+for sheet_name, df in raw_stock_dfs.items():
+    # rename columns
+    df.columns = RENAMED_COLUMNS
+    # cast data
+    df['date'] = pd.to_datetime(df['date'])
+    # if name have char * replace with _
+    df['name'] = df['name'].str.replace('*', '_')
+
+# make each stock a df
+stock_dfs = dict()
+for sheet_name, df in raw_stock_dfs.items():
+    # make each stock a sheet
+    for stock_id in tqdm(df['name'].unique()):
+        stock_df = df[df['name'] == stock_id]
+        # if stock_dfs have stock_id, stack it
+        if stock_id in stock_dfs:
+            stock_dfs[stock_id] = pd.concat([stock_dfs[stock_id], stock_df])
+        else:
+            stock_dfs[stock_id] = stock_df
+
+# order by time
+for stock_id, df in stock_dfs.items():
+    stock_dfs[stock_id] = df.sort_values(by='date')
+
+# Now we make metadata for all stock, this should include row_cnt, have_null_price, missing_rows_cnt
+# make metadata for all stock
+stock_metadata = dict()
+for stock_id, df in stock_dfs.items():
+    # get row count
+    row_cnt = df.shape[0]
+    # check if there is any null price
+    have_null_price = df['close'].isnull().any()
+    stock_metadata[stock_id] = {
+        "id": stock_id.split(' ')[0],
+        "name": stock_id.split(' ')[1],
+        'row_cnt': row_cnt,
+        'have_null_price': have_null_price,
+        "history_path": "",
+    }
+# check type 上市 or 上櫃
+for sheet_name, df in raw_stock_dfs.items():
+    stock_type = "上市" if sheet_name == "上市股票" else "上櫃"
+    for stock_id in df['name'].unique():
+        stock_metadata[stock_id]['stock_type'] = stock_type
+# convert to df
+stock_metadata_df = pd.DataFrame(stock_metadata).T
+
+for stock_id, df in stock_dfs.items():
+    stock_dfs[stock_id] = stock_dfs[stock_id].drop(columns=['name'])
+
+
+# Save metadata and stock dfs csv files
+# save to csv
+os.makedirs(STOCKS_DIR, exist_ok=True)
+for stock_id, df in tqdm(stock_dfs.items()):
+    csv_file_path = Path(ORGANIZED_DATASET_DIR, f"{stock_id}.csv")
+    stock_metadata_df.loc[stock_id, 'history_path'] = str(csv_file_path.absolute())
+    df.to_csv(csv_file_path, index=False)
+metadata_csv_path = Path(METADATA_PATH)
+stock_metadata_df.to_csv(metadata_csv_path, index=False)
