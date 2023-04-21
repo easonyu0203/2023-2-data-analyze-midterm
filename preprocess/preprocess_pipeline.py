82542fcb6480d3433eeee875d70da7117ff91b0c
easonyu0203 <breaking0203@gmail.com>
2023-04-22 03:28:20 +0800

    refactor: change the interface for preprocess relate class

--- preprocess/preprocess_pipeline.py
+++ preprocess/preprocess_pipeline.py
@@ -13,13 +13,11 @@ from dataclasses import dataclass
 from typing import List
 
 from datasets.docs_dataset import IDocsDataset
-from datasets.document import Document
 from datasets.labeled_docs_dataset import ILabeledDataset
 from datasets.stock_dataset import Stock
 from preprocess.docs_filterer import IDocsFilterer
 from preprocess.docs_labeler import IDocsLabeler
 from preprocess.keyword_extractor import IKeywordExtractor
-from preprocess.labeled_docs_filterer import ILabeledDocsFilterer
 from preprocess.vectorlizer import IVectorlizer
 
 
@@ -27,9 +25,9 @@ from preprocess.vectorlizer import IVectorlizer
 class PreprocessPipeLineConfig:
     docs_filterer: IDocsFilterer
     docs_labeler: IDocsLabeler
-    labeled_docs_filterer: ILabeledDocsFilterer
     keywords_extractor: IKeywordExtractor
     vectorizer: IVectorlizer
+    fit_vectorizer: bool = True
 
 
 class PreprocessPipeline:
@@ -37,43 +35,31 @@ class PreprocessPipeline:
     def __init__(self, config: PreprocessPipeLineConfig):
         self.config = config
 
-    def print_line(self, verbose=True):
-        if verbose:
-            print()
-            print("=" * 30)
-
     def preprocess(self, docs_dataset: IDocsDataset, stock: Stock, verbose=True) -> ILabeledDataset:
         """main preprocess pipe line function"""
 
         self.print_line(verbose=verbose)
         # filter documents
-        filtered_docs = self.config.docs_filterer.filter_documents(docs_dataset, stock,
-                                                                   verbose=verbose)
+        filtered_docs = self.config.docs_filterer.filter_documents(docs_dataset, stock, verbose=verbose)
 
         self.print_line(verbose=verbose)
         # label documents
-        labeled_docs = self.config.docs_labeler.label_documents(filtered_docs, stock, verbose=verbose)
-
-        self.print_line(verbose=verbose)
-        # filter labeled documents
-        filtered_labeled_docs = self.config.labeled_docs_filterer.filter_documents(labeled_docs, verbose=verbose)
+        docs_dataset = self.config.docs_labeler.label_documents(filtered_docs, stock, verbose=verbose)
 
         self.print_line(verbose=verbose)
         # extract keywords
-        labeled_docs_keywords = self.config.keywords_extractor.extract_keywords(filtered_labeled_docs, verbose=verbose)
+        keyword_docs_dataset = self.config.keywords_extractor.extract_keywords(docs_dataset, verbose=verbose)
 
         self.print_line(verbose=verbose)
+        # fit vectorizer
+        if self.config.fit_vectorizer:
+            self.config.vectorizer.fit(keyword_docs_dataset, verbose=verbose)
         # convert keywords to vectors
-        labeled_docs_vectors = self.config.vectorizer.convert(labeled_docs_keywords, verbose=verbose)
+        vector_dataset = self.config.vectorizer.transform(keyword_docs_dataset, verbose=verbose)
 
-        return labeled_docs_vectors
+        return vector_dataset
 
-    def transform(self, doc: Document) -> List[float]:
-        keywords, label = self.config.keywords_extractor.extract_keywords()
-
-    def single_doc_transform(self, doc: Document):
-        """transform single document, this is useful when want to do inference, we want to have some transform when
-        training"""
-        keywords = self.config.keywords_extractor.transform(doc)
-        vector = self.config.vectorizer.transform(keywords)
-        return vector
+    def print_line(self, verbose=True):
+        if verbose:
+            print()
+            print("=" * 30)
